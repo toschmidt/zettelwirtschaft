30856c708b6521439a90d004b4bb6476ff30170e
Tobias Schmidt <tobias.schmidt@in.tum.de>
2020-04-23 21:44:40 +0200

    remove example model

--- packages/types/src/models/example.model.ts
+++
@@ -1,8 +0,0 @@
-import { Required } from '@tsed/common';
-import { Description } from '@tsed/swagger';
-
-export class ExampleModel {
-  @Description('Message')
-  @Required()
-  message!: string;
-}
