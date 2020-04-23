fb5787743cb659adce8c8a80253b68a44b2a516f
Tobias Schmidt <tobias.schmidt@in.tum.de>
2020-04-23 22:18:35 +0200

    remove dependencies to example model and controller

--- packages/frontend/src/components/example.component.tsx
+++ packages/frontend/src/components/example.component.tsx
@@ -1,18 +1,7 @@
-import { ExampleModel } from '@zettelwirtschaft/types';
 import * as React from 'react';
-
-import { useGetExamples } from '../repositories/example.repository';
 
 export interface ExampleProps {}
 
 export const ExampleComponent = (): React.ReactElement => {
-  const { data: examples, error } = useGetExamples();
-
-  return (
-    <>
-      {!error &&
-        examples &&
-        examples.map((example: ExampleModel, index: number) => <p key={index}>{example.message}</p>)}
-    </>
-  );
+  return <></>;
 };
--- packages/frontend/src/repositories/example.repository.ts
+++
@@ -1,8 +0,0 @@
-import { ExampleModel } from '@zettelwirtschaft/types';
-import { useGet, UseGetReturn } from 'restful-react';
-
-const apiPath = '/examples';
-
-export function useGetExamples(): UseGetReturn<ExampleModel[], string> {
-  return useGet<ExampleModel[]>({ path: apiPath });
-}
